1c147913d3f79b850a6e013781709fd5d52150a0
wenhaohaoo <lwen.hao93@gmail.com>
2021-10-24 22:15:28 +0800

    change to logging and add resume option

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import ast
-import json
 import time
 import datetime
 import argparse
+import logging
 from pathlib import Path
 
 from selenium import webdriver
@@ -13,11 +13,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from tqdm import tqdm
 
 
+log_format = '[%(asctime)s] [%(levelname)s] - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_format)
+
 def init_driver():
     chromedriver = Service(ChromeDriverManager().install())
 
     options = Options()
     options.add_argument('--headless')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--start-maximized')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-web-security')
@@ -25,7 +30,6 @@ def init_driver():
     options.add_argument('--allow-cross-origin-auth-prompt')
 
     driver = webdriver.Chrome(service=chromedriver, options=options)
-    driver.maximize_window()
     return driver
 
 
@@ -52,7 +56,7 @@ def scroll_down(driver, wait_time):
 
             last_height = new_height
 
-    print('Scrolling down ..')
+    logging.info('Scrolling down ..')
     while True:
         _scroll()
         try:
@@ -62,24 +66,23 @@ def scroll_down(driver, wait_time):
             # Wait for page to load
             time.sleep(wait_time)
         except:
-            print('Reached the end ..')
+            logging.info('Reached the end ..')
             
             # Scroll back to top
             driver.execute_script("window.scrollTo(0,0)")
             return
 
 
-def get_image_url(driver, search_term, dev=False, output_root=Path('./')):
+def get_image_url(driver, search_term, dev=False, output_root=Path('./'), resume=None):
 
-    print(f'Search term: {search_term}')
+    logging.info(f'Search term: {search_term}')
 
-    images = {}
+    images = set()
     try:
-        with open(output_root/f'{search_term}.json', 'r') as f:
-            images = json.loads(f.read())
-            images['date'] = str(datetime.datetime.now())
+        with open(output_root/f'{search_term}.txt', 'r') as f:
+            images = ast.literal_eval(f.read())
     except FileNotFoundError:
-        print('No prevous results found, starting new search')
+        logging.info('No prevous results found, starting new search')
         pass
 
     base_url = f'https://www.google.com/search?q={search_term}&tbm=isch&ijn=0'
@@ -91,18 +94,36 @@ def get_image_url(driver, search_term, dev=False, output_root=Path('./')):
     # Get list of image thumbnails to extract
     parent_elem = driver.find_element(By.XPATH, '/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div[1]/div[1]/span/div[1]/div[1]')
     all_children = parent_elem.find_elements(By.XPATH, '*')
-    print(f'Total results found: {len(all_children)}')
+    logging.info(f'Total results found: {len(all_children)}')
 
-    def _scrape(list, retry=False, images = {'date': str(datetime.datetime.now())}, dev=False):
+    def _save(images):
+        with open(output_root/f'{search_term}.txt', 'w') as f:
+            f.write(str(images))
+
+    def _scrape(list, retry=False, images=set(), dev=False, resume=None):
+
+        logging.info(f'is_retry: {not retry}')
+        logging.info(f'dev: {dev}')
+
+        original_len = len(list)
+        starting_pt = 0
 
         if dev:
-           list = list[:50]
+           list = list[:20]
 
-        image_set = set(images.values())
+        if resume:
+            try:
+                resume = int(resume)
+                list = list[resume:]
+                starting_pt = resume
+                logging.info(f'Resuming from {resume}')
+            except Exception as e:
+                logging.error(f'resume failed: {e}')
+                exit(1)
 
         retry_list = []
 
-        for idx, img in enumerate(tqdm(list, desc=f'search_term={search_term} | is_retry={not retry} ')):
+        for idx, img in enumerate(list):
             try:
                 # Filter for only image thumbnails
                 if img.get_attribute('class') == 'isv-r PNCib MSM1fd BUooTd':
@@ -117,30 +138,33 @@ def get_image_url(driver, search_term, dev=False, output_root=Path('./')):
 
                     # Retrieve 'src' attribute from the element
                     img_src = image.get_attribute('src')
-                    if img_src.startswith('http') and img_src not in image_set:
-                        images[idx] = img_src
-                        image_set.add(img_src)
+                    if img_src.startswith('http') and img_src not in images:
+                        images.add(img_src)
 
             except Exception as e:
                 if retry:
-                    print(f'appending {idx} to retry list')
+                    logging.warning(f'failed, appending to retry list: {idx}, {e}')
                     retry_list.append(all_children[idx])
                 pass
+
+            if idx%10 == 0:
+                _save(images)
+
+            logging.info(f'{starting_pt+idx+1}/{original_len}')
         
         return images, retry_list
-    
+
     # First pass
-    images, retry_list = _scrape(all_children, retry=True, dev=True)
-    print(f'Total image URLs extracted in first pass: {len(images)}')
+    images, retry_list = _scrape(all_children, images=images, retry=True, dev=dev, resume=resume)
+    logging.info(f'Total image URLs extracted in first pass: {len(images)}')
 
     # Retry those with ElementClickInterceptedException 
     if retry_list:
         images, _ = _scrape(retry_list, images=images)
-        print(f'Total image URLs extracted after retrying: {len(images)}')
+        logging.info(f'Total image URLs extracted after retrying: {len(images)}')
 
-    # Write URLs to json file
-    with open(output_root/f'{search_term}.json', 'w') as f:
-        f.write(json.dumps(images))
+    # Write URLs to txt file
+    _save(images)
 
 
 if __name__ == "__main__":
@@ -155,9 +179,17 @@ if __name__ == "__main__":
         default='./',
         help='output root directory',
     )
+    parser.add_argument(
+        '-r',
+        default=None,
+        help='index to resume from',
+    )
 
     args = parser.parse_args()
     driver = init_driver()
     Path(args.o).mkdir(parents=True, exist_ok=True)
-    get_image_url(driver, args.s, dev=True, output_root=Path(args.o))
+    if not args.r:
+        get_image_url(driver, args.s, dev=False, output_root=Path(args.o))
+    else:
+        get_image_url(driver, args.s, dev=False, output_root=Path(args.o), resume=args.r)
     driver.close()
