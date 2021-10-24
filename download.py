4bd747196cbab30a5771f6d6afd874cdaa9b12e6
wenhaohaoo <lwen.hao93@gmail.com>
2021-10-24 22:16:56 +0800

    supress warning and read from set instead of json

--- download.py
+++ download.py
@@ -1,29 +1,32 @@
-import json
+import ast
 import argparse
 import requests
+import urllib3
 from pathlib import Path
 
 from tqdm import tqdm
+
+
+urllib3.disable_warnings()
 
 def download(file_name, output_root=Path('./')):
     print(f'Search term: {file_name}')
 
     (output_root/f'images/{file_name}').mkdir(parents=True, exist_ok=True)
-    with open(output_root/f'{file_name}.json') as f:
-        images = json.loads(f.read())
+    with open(output_root/f'{file_name}.txt') as f:
+        images = ast.literal_eval(f.read())
     
-    for k, v in tqdm(images.items(), desc=f'search_term={file_name} '):
+    for i, v in enumerate(tqdm(images, desc=f'search_term={file_name} ')):
         name = v.split('/')[-1]
         if not (name.endswith('.png') or name.endswith('.jpg') or name.endswith('.jpeg')):
             name += '.jpg'
         
-        if k != 'date': 
-            r = requests.get(v, verify=False)
-            if r.status_code == 200:
-                with open(output_root/f'images/{file_name}/{name}', 'wb') as f:
-                    f.write(r.content)
-            else:
-                print(f'failed: {k}, {v}, {r.status_code}')
+        r = requests.get(v, verify=False)
+        if r.status_code == 200:
+            with open(output_root/f'images/{file_name}/{name}', 'wb') as f:
+                f.write(r.content)
+        else:
+            print(f'failed: {i}, {v}, {r.status_code}')
 
 
 if __name__ == "__main__":
